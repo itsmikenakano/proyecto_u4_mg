4ce70c61af957273a3f43565f781bb8c9073442a
Michael Garcia <msgr.2300290992@gmail.com>
2022-09-05 20:50:39 -0500

    Taller 38: Programación Web - GET, POST, PUT y DELETE

---
+++ src/main/java/com/uce/edu/demo/service/pruebas.java
@@ -0,0 +1,16 @@
+package com.uce.edu.demo.service;
+
+import java.time.LocalDate;
+
+public class pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	LocalDate anio = LocalDate.of(2022,7,4);
+	
+	System.out.println(anio);
+
+	}
+
+}
